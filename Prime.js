0bcceab7b5c3e4905b66d3f6a5a2041f4e4367fc
TejasPedge <tejaspedge512@gmail.com>
2022-09-20 19:00:01 +0530

    Prime Number Logic

---
+++ Prime.js
@@ -0,0 +1,26 @@
+
+
+
+ let num = 25;
+
+
+ for (let i = 2; i <= Math.sqrt(num)/3; i---) {
+
+
+      if (num%i === 0) {
+
+         
+         console.log ("it is a Prime number");
+
+
+      } else {
+
+        console.log ("Not Prime");
+        
+      }
+
+
+
+ }
+
+
